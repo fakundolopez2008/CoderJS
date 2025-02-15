cc793c0fdb8721f2c14157d40d887838673858f6
Facundo Lopez Ordoqui <fakulodornequi@hotmail.com.ar>
2025-02-14 22:38:49 -0300

    Correccion Cases

--- JS/script.js
+++ JS/script.js
@@ -24,13 +24,13 @@ switch (opcion) {
         suma(num1,num2);
         break;
     case "2":
-        suma(num1,num2);
+        resta(num1,num2);
         break;
     case "3":
-        suma(num1,num2);
+        multiplicacion(num1,num2);
         break;
     case "4":
-        suma(num1,num2);
+        division(num1,num2);
         break;    
     default:
         if (opcion > 4){
