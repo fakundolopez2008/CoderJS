d41a62015060f5ef7149c65794cd02e9a1f742ec
Facundo Lopez Ordoqui <fakulodornequi@hotmail.com.ar>
2025-02-15 03:14:10 -0300

    Adicion Varios

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,38 @@
 function suma (numero1,numero2){
     let adicion = numero1 + numero2;
+    resultados.push(adicion);
     console.log ("El resultado es: " + adicion)
 }
 function resta (numero1,numero2){
-    let sustraccion = numero1 - numero2;
-    console.log ("El resultado es: " + sustraccion)
+    let substraccion = numero1 - numero2;
+    resultados.push(substraccion);
+    console.log ("El resultado es: " + substraccion)
 }
 function multiplicacion (numero1,numero2){
     let complemento = numero1 * numero2;
+    resultados.push(complemento);
     console.log ("El resultado es: " + complemento)
 }
-function division (numero1,numero2){
+function division(numero1, numero2) {
     if (numero2 === 0) {
         alert("Error: No se puede dividir por 0");
-    } else if ((numero2 != 0)){
-    let resto = numero1 / numero2;
-    console.log ("El resultado es: " + resto)
+    } else {
+        let resto = numero1 / numero2;
+        resultados.push(resto);
+        console.log("El resultado es: " + resto);
     }
 }
 
+let resultados = [];
+
 let num1 = parseFloat(prompt("Ingresar Primer Número"));
 let num2 = parseFloat(prompt("Ingresar Segundo Número"));
-let opcion = prompt("Que calculo desea realizar?\n1 - Sumar\n2- Restar\n3- Multiplicar\n4- Dividir\n5- Salir");
 
-while (opcion <= 4){
+let opcion = "";
+
+while (opcion !== "7") {
+
+    opcion = prompt("¿Qué cálculo desea realizar?\n1 - Sumar\n2 - Restar\n3 - Multiplicar\n4 - Dividir\n5 - Cargar Nuevos Números\n6 - Ver Resultados\n7 - Salir");
     
     switch (opcion) {
         case "1":
@@ -38,12 +47,26 @@ while (opcion <= 4){
         case "4":
             division(num1,num2);
             break;    
+        case "5":
+            num1 = parseFloat(prompt("Ingresar Primer Número"));
+            num2 = parseFloat(prompt("Ingresar Segundo Número"));
+            console.log("Los nuevos números cargados son: " + num1 + " y " + num2);
+            break; 
+        case "6":
+            if (resultados.length === 0){
+                console.log("No se realizaron operaciones");
+            } else {
+                for(let i = 0; i < resultados.length; i++){
+                    console.log ("Los resultados registrados son los siguientes: " + resultados[i]+"\n"); 
+                } 
+            }                
+            break; 
+        case "7":
+            alert("Programa Finalizado");
+            console.log("Gracias por usar Calculator3000");
+            break;
         default:
-            break;        
+            console.log("Opción no válida. Intente de nuevo.");
+            break;       
     }
-    opcion = prompt("Que calculo desea realizar?\n1 - Sumar\n2- Restar\n3- Multiplicar\n4- Dividir\n5- Salir");
 }
-if (opcion > 4){
-    alert ("Programa Finalizado");
-    console.log("Gracias por usar Calculator3000");
-} 
